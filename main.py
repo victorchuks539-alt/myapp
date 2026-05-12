f728514dcbba462988b9d49bdd5a5f257fe08a83
victor-88-chuks <victorchuks539@gmail.comme>
2026-05-12 12:03:15 +0100

    updated the tkinter app

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter
+from tkinter import *
+win=Tk()
+win.config(background="khaki")
+win.title("opay")
+win.geometry("750x600")
+lab=Label(win,text="welcome Victor",bg="teal",font=("cooper black",20,"bold"))
+lab.place(relx=0.5,rely=0.2,anchor=CENTER)
+win.mainloop()         
